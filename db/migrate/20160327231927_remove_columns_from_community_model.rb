136dbcdb345d9f64e9efc6652bd041e3399c762c
Nathan Justice <n.justice@outlook.com>
2016-04-03 21:30:08 -0400

    updated migration

--- db/migrate/20160327231927_remove_columns_from_community_model.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromCommunityModel < ActiveRecord::Migration
-  def change
-  	add_column :communities, :city, :text
-  	add_column :communities, :state, :text
-  	remove_column :communities, :city
-  	remove_column :communities, :state
-  end
-end
