1d27712bf78e103458394248cab750338f76eae4
Nathan Justice <n.justice@outlook.com>
2016-04-03 21:40:31 -0400

    updating migrations

--- db/migrate/20160328000318_updating_community_foreign_key.rb
+++
@@ -1,7 +0,0 @@
-class UpdatingCommunityForeignKey < ActiveRecord::Migration
-  def change
-  	remove_index :users, :community_id
-  	add_column :users, :community_name, :text
-  	add_index :users, :community_name
-  end
-end
