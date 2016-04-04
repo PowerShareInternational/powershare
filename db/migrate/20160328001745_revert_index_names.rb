d466f0bd586b9e177045d5bd0f425b33d4441123
Nathan Justice <n.justice@outlook.com>
2016-04-03 21:43:18 -0400

    updating migrations

--- db/migrate/20160328001745_revert_index_names.rb
+++
@@ -1,7 +0,0 @@
-class RevertIndexNames < ActiveRecord::Migration
-  def change
-  	remove_index :users, :community_name
-  	add_column :users, :community_id, :integer
-  	add_index :users, :community_id
-  end
-end
